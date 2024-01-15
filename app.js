b84df17c124a2465b66ac5d62e243e6c5de091c5
nurfznhanif <99284772+nurfznhanif@users.noreply.github.com>
2024-01-15 23:38:05 +0700

    Tugas DPM

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const mongoUrl = "mongodb+srv://eliteisan:admin@dpm2023.arvk2ot.mongodb.net/?retryWrites=true&w=majority";
+const mongoUrl = "mongodb+srv://nrfznhnf:misterhan19@cluster0.fngxe3r.mongodb.net/?retryWrites=true&w=majority";
 
 const JWT_SECRET = "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jdsds039[]]pou89ywe";
 
